3586ffb7611dab9a9667d746edfaa7a4c23c9920
Jora-Harsh <harshjora2004@gmail.com>
2025-08-26 19:43:31 +0530

    again inngest foldar name change

--- server/server.js
+++ server/server.js
@@ -1,23 +1,29 @@
-// server/server.js
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express';
-import { serve } from 'inngest/express';
-import { inngest, functions } from './inngest/index.js';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
+const port = 3000;
 
-await connectDB();
+await connectDB()
 
-// middleware
-app.use(express.json());
-app.use(cors());
-app.use(clerkMiddleware());
 
-// routes
-app.get('/', (req, res) => res.send('Server Is Live'));
-app.use('/api/inngest', serve({ client: inngest, functions }));
+//MiddleWare
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
 
-export default app;        
+
+// API routes
+app.get('/', (req, res)=> res.send('Server Is Live'))
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+app.listen(port, ()=> console.log(`Server Listening at http://localhost:${port}`))
+
+
+
